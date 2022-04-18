5a6f85f06aeb38e5f8be75c852b2415ea66716d7
Bechir Majris <bmajri@gmail.com>
2022-04-18 17:19:03 +0200

    card ok

--- frontend/src/components/card.vue
+++ frontend/src/components/card.vue
@@ -1,5 +1,5 @@
 <script>
-export default{
+export default {
   name: 'card'
 }
 
@@ -7,82 +7,90 @@ export default{
 
 <template>
 
-<div>
-  <div class="card mb-3" style="max-width: 540px;">
-        <div class="card-header">@pseudo</div>
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="../../public/icon.svg" class="img-fluid rounded-start" alt="...">
+  <div>
+    <div class="card mb-3" style="max-width: 540px;">
+      <div class="card-header">@pseudo</div>
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="../../public/icon.svg" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Commentaire</h5>
+            <p class="card-text">Mon commentaire c'est sa.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <button type="button" class="btn btn-outline-primary">👍</button>
+            <button type="button" class="btn btn-outline-danger">👎</button>
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Commentaire</h5>
-        <p class="card-text">Mon commentaire c'est sa.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-       <button type="button" class="btn btn-outline-primary">👍</button>  
-       <button type="button" class="btn btn-outline-danger">👎</button> 
+
+    <div class="card mb-3" style="max-width: 540px;">
+      <div class="card-header">@pseudo</div>
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="../../public/icon.svg" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Commentaire</h5>
+            <p class="card-text">Mon commentaire c'est sa.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            <button type="button" class="btn btn-outline-primary">👍</button>
+            <button type="button" class="btn btn-outline-danger">👎</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="card mb-3" style="max-width: 540px;">
+      <div class="card-header">@pseudo</div>
+      <div class="row g-0">
+        <div class="col-md-4">
+          <img src="../../public/icon.svg" class="img-fluid rounded-start" alt="...">
+        </div>
+        <div class="col-md-8">
+          <div class="card-body">
+            <h5 class="card-title">Commentaire</h5>
+            <p class="card-text">Mon commentaire c'est sa.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+        <div class="buttonLikes">
+          <button type="button" class="btn btn-outline-primary">👍</button>
+          <button type="button" class="btn btn-outline-danger">👎</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
-
-<div class="card mb-3" style="max-width: 540px;">
-        <div class="card-header">@pseudo</div>
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="../../public/icon.svg" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Commentaire</h5>
-        <p class="card-text">Mon commentaire c'est sa.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-       <button type="button" class="btn btn-outline-primary">👍</button>  
-       <button type="button" class="btn btn-outline-danger">👎</button> 
-      </div>
-    </div>
-  </div>
-</div>
 
 
-<div class="card mb-3" style="max-width: 540px;">
-        <div class="card-header">@pseudo</div>
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="../../public/icon.svg" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Commentaire</h5>
-        <p class="card-text">Mon commentaire c'est sa.</p>
-        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-       <button type="button" class="btn btn-outline-primary">👍</button>  
-       <button type="button" class="btn btn-outline-danger">👎</button> 
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-
-  
 
 </template>
 
 <style scoped>
-.statusButton{
+.statusButton {
   margin: 0 auto;
-display: flex;
-justify-content: center;
+  display: flex;
+  justify-content: center;
 }
-.card{
+
+.card {
   width: 100%;
   margin: 50px;
 
 }
 
-img{
+.buttonLikes {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+}
+
+img {
   object-fit: cover;
 
 }
-
 </style>
--- frontend/src/components/header.vue
+++ frontend/src/components/header.vue
@@ -6,18 +6,25 @@ export default {
 </script>
 
 <template>
-   <header class="p-3 headerColor text-white">
+  <header class="p-3 headerColor text-white">
 
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-         <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-          <svg class="bi me-3" width="50" height="50" role="img" aria-label="groupomania"><use xlink:href="../../public/Groupomania_Logos+(3)/icon-left-font-monochrome-black.svg"/></svg>
+        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <svg class="bi me-3" width="50" height="50" role="img" aria-label="groupomania">
+            <use xlink:href="../../public/Groupomania_Logos+(3)/icon-left-font-monochrome-black.svg" />
+          </svg>
         </a>
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><router-link to="/home" class="nav-link  px-3 btn text-white">Home</router-link></li>
-          <li><router-link to="/profile" class="nav-link  px-3 btn text-white">Profile</router-link></li>
-          <li><router-link to="/profile/amis" class="nav-link  px-3 btn text-white">Amis</router-link></li>
-          <li><router-link to="/contact" class="nav-link  px-3 btn text-white">Contact</router-link></li>
+          <li>
+            <router-link to="/home" class="nav-link  px-3 btn text-white">Home</router-link>
+          </li>
+          <li>
+            <router-link to="/profile" class="nav-link  px-3 btn text-white">Profile</router-link>
+          </li>
+          <li>
+            <router-link to="/profile/amis" class="nav-link  px-3 btn text-white">Amis</router-link>
+          </li>
         </ul>
 
         <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
@@ -25,30 +32,34 @@ export default {
         </form>
 
         <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2 "> <router-link to="/login" class="link-light text-decoration-none">  Login</router-link></button>
-          <button type="button" class="btn btn btn-outline-light btn-primary "> <router-link to="/singup" class="link-light text-decoration-none">Sign-up </router-link></button>
+          <button type="button" class="btn btn-outline-light me-2 ">
+            <router-link to="/login" class="link-light text-decoration-none"> Login</router-link>
+          </button>
+          <button type="button" class="btn btn btn-outline-light btn-primary ">
+            <router-link to="/singup" class="link-light text-decoration-none">Sign-up </router-link>
+          </button>
         </div>
       </div>
     </div>
-  </header> 
+  </header>
 </template>
 
 <style scoped>
 .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  user-select: none;
+}
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
+@media (min-width: 768px) {
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
+}
 
-      .b-example-divider {
+.b-example-divider {
   height: 3rem;
   border: solid rgba(0, 0, 0, .15);
   border-width: 1px 0;
@@ -59,6 +70,7 @@ export default {
   color: #fff;
   border-color: var(--bs-gray);
 }
+
 .form-control-dark:focus {
   color: #fff;
   background-color: var(--bs-dark);
@@ -79,7 +91,7 @@ export default {
   outline: 0;
 }
 
-.headerColor{
+.headerColor {
   background-color: #FD2D01;
 }
 </style>
