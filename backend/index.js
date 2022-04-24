94b11b0604e560e4a26096d9bcc2abf2b9e36a58
Bechir Majris <bmajri@gmail.com>
2022-04-25 00:57:20 +0200

    debut du backend, serveur en place

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config(); // je requier dotenv (gestion des variables Environement)
 const express = require("express"); // je requier expresse et je la lance dans app (pour lancer un serveur
+const PORT = process.env.PORT || 3000
 const app = express();
 
-const PORT = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   res.send('Hello World');
