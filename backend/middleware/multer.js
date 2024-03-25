95b726db18a9b6c33b5baf9124b07a27bf8bb837
Bechir Majris <bmajri@gmail.com>
2024-03-25 09:59:14 +0100

    ajout des dependances

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,22 +1,41 @@
 const multer = require("multer");
-console.log("MODULE MULTER.js");
-// dictionnaire
+const path = require("path");
+const fs = require("fs");
+
 const MIME_TYPES = {
   "image/jpg": "jpg",
   "image/jpeg": "jpg",
   "image/png": "png",
 };
+
+const IMAGES_DIR = path.join(__dirname, "..", "images");
+
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
-    callback(null, "../images/");
+    // Vérifiez si le répertoire d'images existe, créez-le s'il n'existe pas
+    if (!fs.existsSync(IMAGES_DIR)) {
+      fs.mkdirSync(IMAGES_DIR);
+    }
+
+    callback(null, IMAGES_DIR);
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
-    console.log(file);
-    // gestion des nom de fichier
+    // Vérifiez si le type de fichier est supporté
+    if (!(file.mimetype in MIME_TYPES)) {
+      return callback(new Error("Unsupported file type"));
+    }
+
+    // Nettoyez le nom de fichier en remplaçant tous les caractères non alphanumériques par des tirets
+    const name = file.originalname.replace(/[^a-zA-Z0-9]/g, "-");
+
+    // Définissez l'extension en fonction du type de fichier
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, `${name}-${Date.now()}.${extension}`); // nom + timestamp + extension
+
+    // Définissez le nom de fichier final
+    const filename = `${name}-${Date.now()}.${extension}`;
+
+    callback(null, filename);
   },
 });
 
-module.exports = multer({ storage }).single("imageUrl");
+module.exports = multer({ storage }).single("image");
